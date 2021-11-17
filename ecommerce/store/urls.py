28b999ca45b9e4a469a19ed835972d07763c01e5
Amal Tenson <amaltenson@gmail.com>
2021-11-18 00:03:35 +0530

    first review

---
+++ ecommerce/store/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    # Leave as empty string for base url
+    path('', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
+]
